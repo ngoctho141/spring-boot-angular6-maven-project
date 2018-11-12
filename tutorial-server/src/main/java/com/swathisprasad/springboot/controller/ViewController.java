2dc39eb45eaedee2140a54249964d1323146bece
Nguyen Ngoc Tho <thofreising141@gmail.com>
2018-11-12 20:41:11 +0100

    config url 2

--- tutorial-server/src/main/java/com/swathisprasad/springboot/controller/ViewController.java
+++ tutorial-server/src/main/java/com/swathisprasad/springboot/controller/ViewController.java
@@ -3,7 +3,6 @@ package com.swathisprasad.springboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Swathi
