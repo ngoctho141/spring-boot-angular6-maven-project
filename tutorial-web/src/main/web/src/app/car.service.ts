b129ddf83153fb1607fc4c28a790b6a495200799
Thọ Nguyễn <thofreising141@gmail.com>
2019-11-14 18:22:03 +0100

    edit userUrl in car service

--- tutorial-web/src/main/web/src/app/car.service.ts
+++ tutorial-web/src/main/web/src/app/car.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
     providedIn: 'root'
 })
 export class CarService {
-    private userUrl = 'http://localhost:8080/api';
+    private userUrl = 'https://tranquil-cliffs-30789.herokuapp.com/api';
 
     constructor(private http: HttpClient) {
     }
